f36c29c23c89790a7b56d36532cc65384f76ec45
Grisha <odnokonnygrisha@gmail.com>
2023-06-12 18:03:56 +0300

    feat: 3Task done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
 double a = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 double b = double.Parse(Console.ReadLine());
