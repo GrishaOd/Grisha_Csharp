ee7d8ca22d315ccfbc69022859c4ca30c9920ad5
Grisha <odnokonnygrisha@gmail.com>
2023-06-12 17:36:38 +0300

    feat: 2Task done

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите первое число: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+double d = double.Parse(Console.ReadLine());
+
+double max = 0;
+if (a > b)
+{
+    if (a > d)
+    {
+        max = a;
+    }
+}   
+else if (b > d)
+{
+    if (b > a)
+    {
+        max = b;
+    }
+}
+else
+{
+    max = d;
+}   
+Console.WriteLine($"Максимальное число: {max}");
